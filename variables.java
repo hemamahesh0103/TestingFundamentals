33c9b16d438a35b0ea9bc3d54203f652984e19f9
classtech235 <classtech235@gmail.com>
2025-02-20 08:42:58 +0530

    Variables In java and Strings

---
+++ variables.java
@@ -0,0 +1,20 @@
+String name1 = "Vamsi";
+System.out.println(name)
+System.out.println(name1);
+Math.pow(2, 3)
+ArrayList<Integer> al = new ArrayList<>();
+name
+name.toUpperCase()
+name
+$8
+String lname = name.toLowerCase();
+name
+$8
+lname
+name1 = "    Vamsi    ";
+name1.trim()
+name1
+$16
+String name = new String("Hema");
+String myName = "Tej";
+int a = 5;
