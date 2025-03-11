65a3573829b48a78355b848a916c456e7bdb9ebb
Heman <nhema0103@gmail.com>
2025-03-10 21:29:46 -0400

    Added Few Files related to Get and Set

---
+++ JavaProgramsPractice/ProgramsPractice/src/main/java/test/programs/classandobjects/cars.java
@@ -0,0 +1,41 @@
+package test.programs.classandobjects;
+
+public class cars{
+    private String name;
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    private String model;
+    private String colour;
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- JavaProgramsPractice/ProgramsPractice/src/main/java/test/programs/interviewexamples/Main.java
+++ JavaProgramsPractice/ProgramsPractice/src/main/java/test/programs/interviewexamples/Main.java
@@ -1,6 +1,9 @@
 package test.programs.interviewexamples;
 
 import test.programs.classandobjects.BankDemo;
+import test.programs.classandobjects.Persons;
+import test.programs.classandobjects.cars;
+import test.programs.classandobjects.dog;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +13,34 @@ public class Main {
         hdfc.setAccountBalance(432323232);
         hdfc.setAccountActive(true);
 
-        System.out.println(hdfc.getAccountName());
+      //  System.out.println(hdfc.getAccountName());
+
+        Persons p = new Persons();
+
+        p.setAge(25);
+        p.setName("Mahesh");
+
+       // System.out.println(p.getAge());
+        // System.out.println(p.getName());
+
+        dog d = new dog();
+        d.setName("German Shepherd");
+        d.setPrice(25000);
+       // System.out.println(d.getName());
+       // System.out.println(d.getPrice());
+
+        cars c = new cars();
+        c.setName("BMW");
+        c.setPrice(200000);
+        c.setModel("3 series");
+        c.setColour("white");
+        System.out.println(c.getName());
+         System.out.println(c.getPrice());
+        System.out.println(c.getModel());
+        System.out.println(c.getColour());
+
+
+
 
 
     }
