f93010b32fceb59df71b1cad79b1648dc645fdde
Heman <nhema0103@gmail.com>
2025-03-21 20:33:38 -0400

    Interface Example

---
+++ JavaProgramsPractice/ProgramsPractice/src/main/java/inheritance/InterfaceExample.java
@@ -0,0 +1,33 @@
+package inheritance;
+//Interface example
+// used interface keyword to define the vehicle
+//created method start and stop
+ interface Vehicle{
+    void stop ();
+      void start();
+ }
+    //created child class bus which implements vehicle
+ class Bus implements Vehicle{
+     // passing strings to print output as bus is starting
+    public void start(){
+         System.out.println("bus is starting");
+     }
+        // passing strings to print output as bus is stopping
+
+
+        public void stop(){
+         System.out.println("bus is stopping");
+     }
+ }
+
+ //created main class InterfaceExample
+
+public class InterfaceExample {
+    public static void main (String args[]){    //main method
+        Vehicle v = new Bus(); //object creation
+        v.start();
+        v.stop();
+
+    }
+
+}
